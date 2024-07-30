30f99486a7943081f3d85b97c0014ed3d3c3df68
Q23RF <elsie094081@gmail.com>
2024-07-30 13:53:39 +0800

    n<=0 cases

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, redirect, request, send_from_directory
+from random import randint
 from poet import write
 from mail import send
 from post import write_on_img, send_line
 
 app = Flask(__name__)
-
+urls = ["https://genshin.hoyoverse.com/zh-tw/?gad_source=1&gclid=Cj0KCQjw-5y1BhC-ARIsAAM_oKkTSAn4TSQ50pyw2CICHlzWVETleLrG869aLrKdAspZmqZJVxzRIYgaAhBdEALw_wcB",
+        "https://youtu.be/dQw4w9WgXcQ?si=yQi6RYOGE7kEJB2h"]
 @app.route('/', methods=["POST", "GET"])
 def main():
     if request.method == "POST":
         if int(request.form["id"])==1:	#寫詩
             n = int(request.form["n"])
             style = int(request.form["style"])
+            if n <= 0:
+                return redirect(urls[randint(0,1)])
             re = write(n, style)
             return render_template('main.html', poem=re)
         else:	#下載
